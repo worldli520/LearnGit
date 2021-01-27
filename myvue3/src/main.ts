7f5a1b92eeeb5b1b61c6ea5cfa6ce255f62b5316
worldLi <1552795695@163.com>
2021-01-27 09:35:12 +0800

    删除

--- myvue3/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import usePlugins from '@/plugins';
-
-const app = createApp(App)
-usePlugins(app)
-app.mount('#app')
-// createApp(App).use(store).use(router).mount('#app')
