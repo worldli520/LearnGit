17191de899a34d07d36ff6f6e91ddc7ee3cdf439
worldLi <1552795695@163.com>
2021-02-02 17:52:13 +0800

    移动合并完成

--- 2048/src/views/2048/index.vue
+++ 2048/src/views/2048/index.vue
@@ -4,10 +4,11 @@
 			<div
 				class="cell"
 				v-for="cell in data"
-				:key="cell.num"
+				:key="cell.id"
 				:class="'x' + cell.x + 'y' + cell.y"
 				:style="{
-					background: cell.color,
+					background: color[cell.num],
+					color: cell.num > 4 ? '#f9f6f2' : '#776e65',
 				}"
 			>
 				{{ cell.num }}
@@ -26,8 +27,8 @@ interface Cell {
 	num: number
 	x: number
 	y: number
-	color: string
 	canMove?: boolean
+	id: number
 }
 
 //矢量数据结构
@@ -41,6 +42,11 @@ interface XY {
 	y: Array<number>
 }
 
+interface Postion {
+	x: number
+	y: number
+}
+
 export default defineComponent({
 	setup() {
 		const color = {
@@ -52,7 +58,7 @@ export default defineComponent({
 			64: '#f65e3b',
 			128: '#edcf72',
 			256: '#edcc61',
-			512: '#0444BF',
+			512: '#edc850',
 			1024: '#A79674',
 			2048: '#282726',
 			4096: '#280b21',
@@ -68,6 +74,7 @@ export default defineComponent({
 			3: { x: -1, y: 0 },
 		}
 		const data = ref<Cell[]>([])
+		const step = ref(0)
 		//新增一个cell
 		const addCell = () => {
 			const num = randomNum()
@@ -75,12 +82,13 @@ export default defineComponent({
 				num: num,
 				x: randomXY(),
 				y: randomXY(),
-				color: color[num],
+				id: step.value,
 			}
 			const _isExist = getCellByXY(cell)
 			if (_isExist) {
 				addCell()
 			} else {
+				step.value++
 				data.value.push(cell)
 			}
 		}
@@ -117,15 +125,71 @@ export default defineComponent({
 
 			//获取矢量方向
 			const vector: Vector = mapVector[direction]
-			console.log(vector)
+			// console.log(vector)
 			//生成当前矢量方向的罗盘（罗盘遍历的第一个元素为移动方向最角上格子）
 			const traversals = buildTraversals(vector)
-			console.log(traversals)
+			// console.log(traversals)
+			let moved: boolean = false
 			traversals.x.forEach((x) => {
 				traversals.y.forEach((y) => {
-					console.log(x, y)
+					const _isExist = getCellByXY({ x: x, y: y })
+
+					if (_isExist) {
+						const position = findFarthestPostion({ x: x, y: y }, vector)
+						// console.log(_isExist)
+						// console.log(position)
+						const next = getCellByXY(position.next)
+						if (next && next.num === _isExist.num && next.canMove) {
+							//聚合Cell
+							const nextIndex = data.value.findIndex((cell) => cell.x === next.x && cell.y === next.y)
+							const positionIndex = data.value.findIndex((cell) => cell.x === _isExist.x && cell.y === _isExist.y)
+							console.log(nextIndex, positionIndex)
+							data.value[nextIndex].num = data.value[nextIndex].num * 2
+							data.value[nextIndex].canMove = false
+							data.value.splice(positionIndex, 1)
+						} else {
+							//移动当前cell到该矢量方向可移动的最远的地方
+							moveCell(_isExist, position.farthest)
+						}
+						if (positionsEqual({ x: x, y: y }, _isExist)) {
+							moved = true
+						}
+					}
 				})
 			})
+			if (moved) {
+				addCell()
+			}
+		}
+
+		//找当前行或列矢量移动方向最远的单元格
+		const findFarthestPostion = (cell: Postion, vector: Vector) => {
+			let previous: Postion = { x: 0, y: 0 }
+			do {
+				previous = cell
+				cell = { x: previous.x + vector.x, y: previous.y + vector.y }
+			} while (withinBounds(cell) && !getCellByXY(cell))
+			return { farthest: previous, next: cell }
+		}
+
+		//判断是否超出边界
+		const withinBounds = (position: Postion) => {
+			return position.x >= 0 && position.x < 4 && position.y >= 0 && position.y < 4
+		}
+
+		//移动单元格
+		const moveCell = (cell: Cell, position: Postion) => {
+			cell.x = position.x
+			cell.y = position.y
+		}
+		// const doubleNum = (cell: Cell) => {
+		// 	cell.num * 2
+		// 	console.log(cell.num)
+		// }
+
+		//是否已经移动过了
+		const positionsEqual = function(first: Postion, second: any) {
+			return first.x === second.x && first.y === second.y
 		}
 
 		//页面挂载元素后新增两个数字
@@ -141,6 +205,7 @@ export default defineComponent({
 		})
 		return {
 			data,
+			color,
 		}
 	},
 })
@@ -162,6 +227,8 @@ export default defineComponent({
 			text-align: center;
 			position: absolute;
 			border-radius: 3px;
+			font-size: 55px;
+			font-weight: 700;
 		}
 	}
 	.background {
@@ -182,69 +249,13 @@ export default defineComponent({
 			border-radius: 3px;
 		}
 	}
-	.x0y0 {
-		top: 16px;
-		left: 16px;
-	}
-	.x1y0 {
-		top: 16px;
-		left: 112px;
-	}
-	.x2y0 {
-		top: 16px;
-		left: 208px;
-	}
-	.x3y0 {
-		top: 16px;
-		left: 304px;
-	}
-	.x0y1 {
-		top: 112px;
-		left: 16px;
-	}
-	.x1y1 {
-		top: 112px;
-		left: 112px;
-	}
-	.x2y1 {
-		top: 112px;
-		left: 208px;
-	}
-	.x3y1 {
-		top: 112px;
-		left: 304px;
-	}
-	.x0y2 {
-		top: 208px;
-		left: 16px;
-	}
-	.x1y2 {
-		top: 208px;
-		left: 112px;
-	}
-	.x2y2 {
-		top: 208px;
-		left: 208px;
-	}
-	.x3y2 {
-		top: 208px;
-		left: 304px;
-	}
-	.x0y3 {
-		top: 304px;
-		left: 16px;
-	}
-	.x1y3 {
-		top: 304px;
-		left: 112px;
-	}
-	.x2y3 {
-		top: 304px;
-		left: 208px;
-	}
-	.x3y3 {
-		top: 304px;
-		left: 304px;
+	@for $i from 0 through 3 {
+		@for $j from 0 through 3 {
+			.x#{$i}y#{$j} {
+				top: #{16 + $j * 96}px;
+				left: #{16 + $i * 96}px;
+			}
+		}
 	}
 }
 </style>
